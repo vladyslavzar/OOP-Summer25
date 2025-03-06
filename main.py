7aa17df307d157e031a2e26e049f7980493e9b68
Vladyslav Zarubin <82956846+vladyslavzar@users.noreply.github.com>
2025-03-06 14:00:18 +0000

    add inheritance implementation

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ class Person:
     self.age = age
     self.adress = adress
 
-vlad = Person('Vlad', 18, 'Grojecka')
 
-print(vlad.name)
-print(vlad.age)
-print(vlad.adress)
+class Student(Person):
+    def __init__(self, name, age, adress, graduation_year):
+        super().__init__( name, age, adress)
+        self.graduation_year = graduation_year
+
+    def introduce_yourself(self):
+       print("My name is " + self.name + ", I am " + str(self.age) + " years old." + " I live on " + self.adress + " street.")
+
+    def state_graduation_year(self):
+       print("My year of graduation is " + str(self.graduation_year))
+
+Vlad = Student("Vlad", 18, "Grojecka", 2028)
+
+Vlad.introduce_yourself()
+Vlad.state_graduation_year()
